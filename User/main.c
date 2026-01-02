846f17e756dee9df5643e0719cfd15a200e0acff
YINGYYUN <756209834@qq.com>
2026-01-02 19:22:02 +0800

    2026.1.2:初版的pid算法和相关调参模块

--- User/main.c
+++ User/main.c
@@ -1,332 +1,209 @@
 #include "stm32f10x.h"  // Device header
+
 #include "Delay.h"
 #include "Timer.h"
+
 #include "LED.h"
-#include "OLED.h"
-#include "Key.h"
 #include "Serial.h"
 #include "Motor.h"
 #include "Encoder.h"
+
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
-#define EPSILON 0.0001
 
-//PID托管标志位,预设为0，在进入对应功能后选择生效
-uint8_t M1_PID_ENABLE = 0, M2_PID_ENABLE = 0;
 
-//PID模式标志位
-//0 定速 ; 1 定位 ;
-//M2的定位模式已经异化为跟随模式
-uint8_t M1_Mode = 0, M2_Mode = 0;
 
-float Target1, Actual1, Out1;
-float Kp1 = 0.5, Ki1 = 0.5, Kd1 = 10;
-float Error01, Error11 ,ErrorInt1;
-int32_t M1_Location = 0;
+//PID相关全局变量
+volatile float Target_1 = 0, Actual_1 = 0, Out_1 = 0;			//目标值，实际值，输出值
+volatile float Kp_1 = 0, Ki_1 = 0, Kd_1 = 0;	//比例项，积分项，微分项的权重
+volatile float Error0_1 = 0, Error1_1 = 0, IntOut_1 = 0;		//本次误差，上次误差，误差积分
 
-float Target2, Actual2, Out2;
-float Kp2 = 2, Ki2 = 0.3, Kd2 = 3;
-float Error02, Error12 ,ErrorInt2;
-int32_t M2_Location = 0;
+volatile float Target_2 = 0, Actual_2 = 0, Out_2 = 0;			//目标值，实际值，输出值
+volatile float Kp_2 = 2, Ki_2 = 0, Kd_2 = 0;		//比例项，积分项，微分项的权重
+volatile float Error0_2 = 0, Error1_2 = 0, IntOut_2 = 0;		//本次误差，上次误差，误差积分
 
-/*项目中心*/
+volatile uint16_t TimeTick = 0;						//计时器（应用在定时器定时中断）
+
 int main()
 {	
+	//模块初始化调用
 	LED_Init();
-	OLED_Init();
-	Key_Init();
 	Serial_Init();
 	Motor_Init();
-	//PWM在Motor模块内初始化并调用
 	Encoder_Init();
-
-	Timer_Init();
-
 	
-	/*初始化菜单及功能1：速度设置*/	
-	uint8_t KeyNum;
+	Serial_Printf("[display-clear]");
+	Serial_Printf("[display,0,0,Tar1   Act1   Out1]");
+	Serial_Printf("[display,0,40,Tar2   Act2   Out2]");
 	
-	//Menu_State表示项目状态 0:电机速度设置 / 1：电机位置跟随
-	uint8_t Menu_State = 0;
-		
-	Motor_SetPWM1(0);
-	Motor_SetPWM2(0);
+	Serial_RxFlag = 0;
 	
-	M1_PID_ENABLE = 1;
-	M2_PID_ENABLE = 0;
-	
-	Serial_SendString("[INFO]READY\r\n");
-	if( Menu_State == 0 )Serial_SendString("[INFO]SPEED SET MODE\r\n");
 	while(1)
 	{
-		//功能切换部分
-		KeyNum = Key_GetNum();
-		if(KeyNum == 1)//按键PA0按下
-		{
-			Menu_State = (Menu_State + 1) % 2;
-			switch(Menu_State)
-			{
-				case 0://定速模式
-					Motor_SetPWM1(0);
-					Motor_SetPWM2(0);
-					M1_PID_ENABLE = 1;//M1自动
-					M2_PID_ENABLE = 0;//M2手动
-					
-					M1_Mode = 0;//M1定速
-				
-					OLED_Clear();
-					OLED_Update();
-				
-					//位置数据重置
-					M1_Location = 0;
-					M2_Location = 0;
-						// 清除PID历史积累，防止模式切换后遗留积分影响输出
-						ErrorInt1 = 0;
-						ErrorInt2 = 0;
-						Error01 = 0;
-						Error02 = 0;
-						Error11 = 0;
-						Error12 = 0;
-				
-					//回传当前状态为：定速模式
-					Serial_SendString("[INFO]SPEED CONTROL MODE\r\n");
-					break;
-				
-				case 1://跟随模式
-					Motor_SetPWM1(0);
-					Motor_SetPWM2(0);
-					M1_PID_ENABLE = 0;//M1手动
-					M2_PID_ENABLE = 1;//M2自动
-					
-					M2_Mode = 1;//M2定位
-				
-					OLED_Clear();
-					OLED_Update();
-				
-					//位置数据重置
-					M1_Location = 0;
-					M2_Location = 0;
-						// 清除PID历史积累，防止模式切换后遗留积分影响输出
-						ErrorInt1 = 0;
-						ErrorInt2 = 0;
-						Error01 = 0;
-						Error02 = 0;
-						Error11 = 0;
-						Error12 = 0;
-				
-					//回传当前状态为：跟随模式
-					Serial_SendString("[INFO]FOLLOWING MODE\r\n");
-					break;
-				
-				default:
-					Motor_SetPWM1(0);
-					Motor_SetPWM2(0);
-					M1_PID_ENABLE = 0;//M1手动
-					M2_PID_ENABLE = 0;//M2手动
-				
-					//回传当前状态为：？？？
-					Serial_SendString("[INFO]???\r\n");
-					break;
-			}
-		}
-		//功能实施部分
-		switch(Menu_State)
+		
+		
+		
+		
+		/* ==================== [START] 蓝牙接收数据处理模块 [START] ==================== */
+		if (Serial_RxFlag == 1)
 		{			
-			case 0://定速模式
-				OLED_Printf(0, 0, OLED_8X16,"Speed Control");
-				OLED_Update();
-				if (Serial_RxFlag == 1)//收到对应格式的文本信息
+			//字符串分割
+			char * Tag = strtok(Serial_RxPacket, ",");
+			
+//			if (strcmp(Tag, "key") == 0)
+//			{
+//				//NULL表示为后续分割
+//				char * Name = strtok(NULL, ",");
+//				//如果没有新的子串，函数会返回空指针
+//				char * Action = strtok(NULL, ",");
+//				
+//				if (strcmp(Name, "1") == 0 && strcmp(Action, "up") == 0)
+//				{
+//					printf("Key,1,up\r\n");
+//				}
+//				else if (strcmp(Name, "2") == 0 && strcmp(Action, "down") == 0)
+//				{
+//					printf("Key,1,down\r\n");
+//				}
+//			}
+//			else 
+			if (strcmp(Tag, "slider") == 0)
+			{
+				//NULL表示为后续分割
+				char * Name = strtok(NULL, ",");
+				//如果没有新的子串，函数会返回空指针
+				char * Value = strtok(NULL, ",");
+				
+				if (strcmp(Name, "Kp1") == 0)
 				{
-					Serial_Printf("[INFO]Received: %s\r\n", Serial_RxPacket);//状态回传上位机
-					if (strstr(Serial_RxPacket, "speed%") != NULL) {
-						int16_t speed;
-						// 从字符串中提取“speed%”后面的数字
-						sscanf(Serial_RxPacket, "speed%%%hd", &speed);
-						Target1 = speed ;
-						if (Target1 >= 100)Target1 = 99;
-						if (Target1 <= -100)Target1 = -99;
-						// 重置积分和历史误差，避免之前的积分项继续驱动电机（积分风暴）
-						ErrorInt1 = 0;
-						Error01 = 0;
-						Serial_Printf("[INFO]Set_Speed:%d\r\n",(int)Target1);//状态回传上位机
-					} else {
-						Serial_SendString("[INFO]ERROR_COMMAND\r\n");//状态回传上位机
-					}
-					Serial_RxFlag = 0;//重置标志位
+					//字符串转换为int
+					float FloatValue = atof(Value);
+					Kp_1 = FloatValue;
 				}
-				//%f显示浮点数，+始终显示正负号，
-				//4显示宽度，0表示数值前面补零，.0表示浮点数保留0位小数
-				
-				//暂时决定同步展示数据
-				OLED_Printf(0, 16, OLED_8X16, "P1:%4.2f", Kp1);
-				OLED_Printf(0, 32, OLED_8X16, "I1:%4.2f", Ki1);
-				OLED_Printf(0, 48, OLED_8X16, "D1:%4.2f", Kd1);
-								
-				OLED_Printf(64, 16, OLED_8X16, "Tar:%+04.0f", Target1);
-				OLED_Printf(64, 32, OLED_8X16, "Act:%+04.0f", Actual1);
-				OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out1);
-				
-				OLED_Update();
-				
-				Serial_Printf("%f,%f,%f\r\n", Target1, Actual1, Out1);		
+				else if (strcmp(Name, "Ki1") == 0)
+				{
+					//字符串转换为double
+					float FloatValue = atof(Value);
+					Ki_1 = FloatValue;
+				}
+				else if (strcmp(Name, "Kd1") == 0)
+				{
+					//字符串转换为double
+					float FloatValue = atof(Value);
+					Kd_1 = FloatValue;
+				}
+				else if (strcmp(Name, "Ta1") == 0)
+				{
+					//字符串转换为double
+					float FloatValue = atof(Value);
+					Target_1 = FloatValue;
+				}
+				else if (strcmp(Name, "Kp2") == 0)
+				{
+					//字符串转换为int
+					float FloatValue = atof(Value);
+					Kp_2 = FloatValue;
+				}
+				else if (strcmp(Name, "Ki2") == 0)
+				{
+					//字符串转换为double
+					float FloatValue = atof(Value);
+					Ki_2 = FloatValue;
+				}
+				else if (strcmp(Name, "Kd2") == 0)
+				{
+					//字符串转换为double
+					float FloatValue = atof(Value);
+					Kd_2 = FloatValue;
+				}
+				else if (strcmp(Name, "Ta2") == 0)
+				{
+					//字符串转换为double
+					float FloatValue = atof(Value);
+					Target_2 = FloatValue;
+				}
+			}
+//			else if (strcmp(Tag, "joystick") == 0)
+//			{
+//				//左摇杆横向值
+//				int8_t LH = atoi(strtok(NULL, ","));
+//				//左摇杆纵向值
+//				int8_t LV = atoi(strtok(NULL, ","));
+//				//右摇杆横向值
+//				int8_t RH = atoi(strtok(NULL, ","));
+//				//右摇杆纵向值
+//				int8_t RV = atoi(strtok(NULL, ","));
+//				
+//				printf("joystick, %d, %d, %d, %d\r\n", LH, LV, RH, RV);
+//			}
 							
-				break;
-			case 1://跟随模式
-				OLED_Printf(0, 0, OLED_8X16,"Following Mode");
-				OLED_Update();
-			
-				//主要功能由PID托管
-			
-				//暂时决定同步展示数据
-			
-				OLED_Printf(0, 16, OLED_8X16, "P2:%4.2f", Kp2);
-				OLED_Printf(0, 32, OLED_8X16, "I2:%4.2f", Ki2);
-				OLED_Printf(0, 48, OLED_8X16, "D2:%4.2f", Kp2);
-			
-				OLED_Printf(64, 16, OLED_8X16, "1:%06d", M1_Location);
-				OLED_Printf(64, 32, OLED_8X16, "2:%06d", M2_Location);
-				OLED_Printf(64, 48, OLED_8X16, "Out:%+04.0f", Out2);
-			
-				OLED_Update();
-			
-				Serial_Printf("%d,%d,%f\r\n", (int)M1_Location, (int)M2_Location, Out2);
-						
-				break;
-			
-			default://????模式
-				//留一手
-				//权当一个彩蛋
-				OLED_ShowString(0, 0, "GI Nod-Krai ???",OLED_8X16);
-				OLED_Update();
-							
-				break;				
+			Serial_RxFlag = 0;
 		}
+		/* ==================== [END] 蓝牙接收数据处理模块 [END] ==================== */
+		
+		
+		
+		
+		/* ==================== [START] 蓝牙回传数据模块 [START] ==================== */
+		Serial_Printf("[display,0,20,%03.1f  %03.1f  %03.1f]", Target_1, Actual_1, Out_1);
+		Serial_Printf("[display,0,60,%03.1f  %03.1f  %03.1f]", Target_2, Actual_2, Out_2);
+		/* ==================== [END] 蓝牙回传数据模块 [END] ==================== */
+		
+		
+		
+		
 	}
 }
 
-//由定时器中断自动执行;有利于多模块共用定时器定时
-		//同时，需要防止中断重叠
-		//一:减小模块内中断函数的内容，减小运行时间
-		//二：增加定时器的基础时间
 //1ms定时器
 void TIM1_UP_IRQHandler(void)
 {
-	static uint16_t Count;
-	
 	//检查标志位
 	if (TIM_GetITStatus(TIM1,TIM_IT_Update) == SET )
 	{
-		Count++;
-		
-		if(Count >= 10)
+		TimeTick ++;
+		if (TimeTick >= 40)
 		{
-
-			Count = 0;
+			TimeTick = 0;
 			
-			Actual1 = Encoder1_Get();
-			Actual2 = Encoder2_Get();
+			//编码器数据读取与初步处理
+			Actual_1 = Encoder1_Get();
+			Actual_2 = Encoder2_Get();
 			
-			M1_Location += Actual1;
-			M2_Location += Actual2;
-
-			if (M1_PID_ENABLE )
-			{
-				if (M1_Mode == 0)//定速模式
-				{
-					Error11 = Error01;
-					Error01 = Target1 - Actual1;
-					
-					ErrorInt1 += Error01;
-					
-					/*输入死区*/
-					if (fabs(Error01) < 2)
-					{
-						Out1 = Kp1 * Error01 * 0.5; 
-					}
-
-					else
-					{
-						
-						/* 积分项累加并限幅（防积分风暴） */
-
-						float IntOut1 = Ki1 * ErrorInt1;
-						/* 把积分输出限制在合理范围，避免单次积分项把输出推到极限 */
-						if (IntOut1 > 100) IntOut1 = 100;
-						if (IntOut1 < -100) IntOut1 = -100;
-
-						Out1 = Kp1 * Error01 + IntOut1 + Kd1 * (Error01 - Error11);
-					}
-					
-					if(Out1 >= 100) {Out1 = 99;}
-					if(Out1 <= -100) {Out1 = -99;}
-					
-					Motor_SetPWM1(Out1);
-				}
-				else if (M1_Mode == 1)
-				{
-					
-				}
-			}
+			Error1_1 = Error0_1;
+			Error0_1 = Target_1 - Actual_1;		
 			
-			if (M2_PID_ENABLE)
-			{
-				if (M2_Mode == 0)//定速模式
-				{
-					
-				}				
-				else if (M2_Mode == 1)//定位（跟随）模式
-				{
-					Error12 = Error02;
-					Error02 = M1_Location - M2_Location;
-					
-					float IntOut2 = 0;
-					
-					if (fabs(Error02) < 10)
-					{
-					// 误差过小（机械抖动/噪声），直接输出0，清零积分
-						Out2 = 0;
-						ErrorInt2 = 0;
-					}
-					else
-					{
-						
-						if (fabs(Error02) > 70)
-						{
-							ErrorInt2 = 0;
-						}
-						else
-						{
-							/* 积分项累加并限幅（防积分风暴） */
-							if ( fabs(Ki2) > EPSILON )
-							{
-								ErrorInt2 += Error02;
-								if (ErrorInt2 > 200) ErrorInt2 = 200;
-								if (ErrorInt2 < -200) ErrorInt2 = -200;
-							}
-							else
-							{
-								ErrorInt2 = 0;
-							}
-							IntOut2 = Ki2 * ErrorInt2;
-							if (IntOut2 > 100) IntOut2 = 100;
-							if (IntOut2 < -100) IntOut2 = -100;						
-						}
-
-						Out2 = Kp2 * Error02 + IntOut2 + Kd2 * (Error02 - Error12);
-						
-						if(Out2 >= 100) {Out2 = 99;}
-						if(Out2 <= -100) {Out2 = -99;}
-					}
-					
-					Motor_SetPWM2(Out2);
-				}
-			}
+			Error1_2 = Error0_2;
+			Error0_2 = Target_2 - Actual_2;
+			
+			//积分限幅
+			IntOut_1 += Ki_1 * Error0_1;
+			if (IntOut_1 >  99){IntOut_1 =  99;}
+			if (IntOut_1 < -99){IntOut_1 = -99;}
+			
+			IntOut_2 += Ki_2 * Error0_2;
+			if (IntOut_2 >  99){IntOut_2 =  99;}
+			if (IntOut_2 < -99){IntOut_2 = -99;}
+			
+			//输出限幅
+			Out_1 = Kp_1 * Error0_1 + IntOut_1 + Kd_1 * (Error0_1 - Error1_1);
+			if (Out_1 >  99){Out_1 =  99;}
+			if (Out_1 < -99){Out_1 = -99;}
+			
+			Out_2 = Kp_1 * Error0_2 + IntOut_2 + Kd_2 * (Error0_2 - Error1_2);
+			if (Out_2 >  99){Out_2 =  99;}
+			if (Out_2 < -99){Out_2 = -99;}
+			
+			//输出
+			Motor_SetPWM1(Out_1);
+			Motor_SetPWM2(Out_2);
+			
 		}
-		//用于Key模块的内部检测
-		Key_Tick();
+		
+		
 		//清除标志位
 		TIM_ClearITPendingBit(TIM1,TIM_IT_Update);
 	}
 }
-
